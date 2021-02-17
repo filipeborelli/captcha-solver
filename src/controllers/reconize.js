276f5f6576a7bece316daff287a6649ab5dc26a5
Filipe Borelli <60223308+filipeborelli@users.noreply.github.com>
2021-02-16 21:56:22 -0300

    Update reconize.js

--- src/controllers/reconize.js
+++ src/controllers/reconize.js
@@ -9,8 +9,12 @@ const solver = async function solver() {
     await worker.loadLanguage('eng');
     await worker.initialize('eng');
     await worker.setParameters({ tessedit_pageseg_mode: PSM.SINGLE_BLOCK, tessedit_char_whitelist: '0123456789', });
-    const { data: { text } } = await worker.recognize(captcha);
-    console.log('The captcha text is: ' + text)
-    return text
+    const resultado = await worker.recognize(captcha).then(result => {
+        let value = result.data.text.trim();
+        return value
+    });
+
+    console.log('The captcha text is: ' + resultado)
+    return resultado
 }
 module.exports = solver;
