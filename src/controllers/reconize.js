65065c8e858a9b7aec7c1826880f231f5101725a
Filipe Borelli <60223308+filipeborelli@users.noreply.github.com>
2021-02-16 21:48:35 -0300

    Create reconize.js

---
+++ src/controllers/reconize.js
@@ -0,0 +1,16 @@
+const { createWorker, PSM } = require('tesseract.js');
+const [, , imagePath] = process.argv;
+const path = require('path');
+const captcha = path.resolve(__dirname, (imagePath || '../images/captcha.png'));
+
+const solver = async function solver() {
+    const worker = createWorker();
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    await worker.setParameters({ tessedit_pageseg_mode: PSM.SINGLE_BLOCK, tessedit_char_whitelist: '0123456789', });
+    const { data: { text } } = await worker.recognize(captcha);
+    console.log('The captcha text is: ' + text)
+    return text
+}
+module.exports = solver;
